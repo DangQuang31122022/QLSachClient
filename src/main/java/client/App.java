85ce86ed4c2104f6881b80c4ba3c7a201e89f286
ThanhNhiet <nhiethiz@gmail.com>
2024-04-06 20:35:17 +0700

    chua load duoc HD

---
+++ src/main/java/client/App.java
@@ -0,0 +1,18 @@
+package client;
+
+import Gui.Login;
+import Gui.QuanLyHoaDon;
+import Gui.TrangChu_QL;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5000);
+//        new Login(socket).setVisible(true);
+        new TrangChu_QL(socket).setVisible(true);
+//        new QuanLyHoaDon(socket).setVisible(true);
+//        System.out.println("check");
+    }
+}
