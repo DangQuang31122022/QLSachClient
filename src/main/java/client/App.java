63ac6e99b65a99b7203b79bc755f18f0e8969050
ThanhNhiet <nhiethiz@gmail.com>
2024-04-25 15:34:28 +0700

    Chinh maHD QLBH

---
+++ src/main/java/client/App.java
@@ -0,0 +1,16 @@
+package client;
+
+import Gui.Login;
+import Gui.QuanLyHoaDon;
+import Gui.TrangChu_NV;
+import Gui.TrangChu_QL;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5000);
+        new Login(socket).setVisible(true);
+    }
+}
